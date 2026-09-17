2df79e694f0b47e98d8043f96cb79052e40130e3
Daniel Baumartz <baumartz@stud.uni-frankfurt.de>
2026-09-17 09:57:44 +0200

    Coh-Metrix new build 0.1.0

--- duui-coh-metrix/src/test/java/org/hucompute/textimager/uima/cohmetrix/CohMetrixDockerValidationTest.java
+++ duui-coh-metrix/src/test/java/org/hucompute/textimager/uima/cohmetrix/CohMetrixDockerValidationTest.java
@@ -55,8 +55,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 @Execution(ExecutionMode.SAME_THREAD)
 public class CohMetrixDockerValidationTest {
+//    static final String DOCKER_IMAGE =
+//            "duui-coh-metrix:review-fixes-20260909";
     static final String DOCKER_IMAGE =
-            "duui-coh-metrix:review-fixes-20260909";
+            "docker.texttechnologylab.org/duui-coh-metrix:0.1.0";
 
     static final String INDEX_TYPE =
             "org.texttechnologylab.uima.type.cohmetrix.Index";
